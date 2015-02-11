1929383140fd2620dad46256a3d6123d3cf330c4
Jay Renteria <jrenteria13@cnm.edu>
2015-02-11 13:29:18 -0700

    beginning personal project

--- index.php
+++ index.php
@@ -1,9 +1,98 @@
 <!DOCTYPE html>
-	<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!--				 CUSTOM CSS-->
+		<link type="text/css" href="css/style.css" rel="stylesheet"/>
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<title>Jay Renteria</title>
 	</head>
 	<body>
-		<p>This is my personal site.</p>
+		<!--		map out the header with navigation-->
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">
+						<img alt="Meese">
+					</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Portfolio</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">More..<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Music</a></li>
+								<li><a href="#">Biography</a></li>
+								<li><a href="#">Gallery</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Contact Me</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+
+		<div class="container">
+
+			<h1>Bootstrap Stuff</h1>
+
+		</div>
+
+		<!--		content above-->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7">First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text </div>
+				<div class="col-md-5">Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text </div>
+			</div>
+
+
+			<!--			Content Below-->
+			<div class="row2">
+				<div class="col-md-5">5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text </div>
+				<div class="col-md-2">2 size filler text 2 size filler text </div>
+				<div class="col-md-5">5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text 5 size filler text </div>
+			</div>
+		</div>
+		<footer class="footer">
+			<div class="container">
+
+				<p class="text-muted">Jay Renteria | Jay@jayrenteria.com</p>
+
+			</div>
+		</footer>
+
 	</body>
-	</html>
+</html>
