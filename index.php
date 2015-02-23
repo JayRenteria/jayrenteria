ce0d7b085305bc64f5c67f7f2300bb6b634d1231
Jay Renteria <jrenteria13@cnm.edu>
2015-02-23 08:22:26 -0700

    added placeholders for pictures and scaled carousel to have all images be the same size

--- index.php
+++ index.php
@@ -67,19 +67,47 @@
 
 		<div class="container">
 
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="images/placeholder.jpg" alt="band photo 1">
+					<div class="carousel-caption">
+						Warehouse 508 (Picture Coming Soon)
+					</div>
+				</div>
+				<div class="item">
+					<img src="images/placeholder.jpg" alt="band photo 2">
+					<div class="carousel-caption">
+						Zinc (Picture Coming Soon)
+					</div>
+				</div>
+			</div>
+
+			<!-- Controls -->
+			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+				</div>
+
 			<h1>Bootstrap Stuff</h1>
 
 		</div>
 
-		<!--		content above-->
+			<!--Content Below-->
 		<div class="container">
-			<div class="row">
-				<div class="col-md-7">First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text First Box Filler Text </div>
-				<div class="col-md-5">Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text Second Box Filler Text </div>
-			</div>
-
-
-			<!--			Content Below-->
 			<div class="row2">
 				<div class="col-md-5">5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text 5 Size Filler Text </div>
 				<div class="col-md-2">2 size filler text 2 size filler text </div>
