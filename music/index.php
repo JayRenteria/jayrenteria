28319fe3b46afa1583df5d9b67e1d7dfa94ad93c
Jay Renteria <jrenteria13@cnm.edu>
2015-02-24 15:57:32 -0700

    added tabs and configured colors

--- music/index.php
+++ music/index.php
@@ -3,3 +3,37 @@ require_once("../php/lib/header.php");
 require_once("../php/lib/footer.php");
 ?>
 <link type="text/css" href="../css/style.css" rel="stylesheet"/>
+
+<div class="container">
+	<div class="content">
+	<ul id="music-nav" class="nav nav-tabs nav-justified navbar-inverse" data-tabs="tabs">
+		<li role="presentation" class="active"><a href="#rogues" data-toggle="tab">Rogues Beware</a></li>
+		<li role="presentation"><a href="#metal" data-toggle="tab">Metal</a></li>
+		<li role="presentation"><a href="#electronic" data-toggle="tab">Electronic</a></li>
+		<li role="presentation"><a href="#sketches" data-toggle="tab">Sketches/WIP</a></li>
+	</ul>
+
+	<div class="tab-content">
+		<div class="tab-pane active" id="rogues">
+			<h1>Rogues Beware</h1>
+			<p>Testing Filler Text</p>
+		</div>
+
+		<div class="tab-pane" id="metal">
+			<h1>Metal</h1>
+			<p>Testing Filler Text</p>
+		</div>
+
+		<div class="tab-pane" id="electronic">
+			<h1>Electronic</h1>
+			<p>Testing Filler Text</p>
+		</div>
+
+		<div class="tab-pane" id="sketches">
+			<h1>Sketches/WIP</h1>
+			<p>Testing Filler Text</p>
+		</div>
+	</div>
+
+	</div>
+</div>
